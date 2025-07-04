ea81b218faef6bfb2f061a26d26617e9f464ee48
Giovanni <dibellogio@gmail.com>
2025-07-04 17:39:03 +0200

    fix routes

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,6 @@ use App\Http\Controllers\Api\GameController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
 Route::get("games", [GameController::class, "index"]);
 
 Route::get("games/{game}", [GameController::class, "show"]);
